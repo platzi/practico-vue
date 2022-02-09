34087db4f285bcd9d7b4b850e85c77024d13933d
Diana Martinez <diananerdoficial@gmail.com>
2022-02-09 15:45:23 -0600

    Add header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <div>Hola mundo</div>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+    </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+
+export default {
+    components: {
+        Layout,
+        Header,
+    }
+};
+</script>
