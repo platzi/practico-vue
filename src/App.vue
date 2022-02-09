3cd5a2b9dac4b9552cf4491c773ba873c9dba531
Diana Martinez <diananerdoficial@gmail.com>
2022-02-08 19:07:04 -0600

    Add splashscreen

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <Home/>
+  <Suspense>
+    <template #default>
+      <Home/>
+    </template>
+    <template #fallback>
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+import { defineAsyncComponent } from "vue";
 
 export default {
   components: {
-    Home,
-  }
+    SplashScreen,
+    Home: defineAsyncComponent(() => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import("./components/Home.vue"))
+        }, 2500);
+      })
+    )
+}
 }
 </script>
 
 <style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="splashscreen">
+        <img
+            src="@/assets/cashflow-icon.svg"
+            alt="Logo"
+        />
+        <h1><span>Cash</span>Flow</h1>
+    </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
