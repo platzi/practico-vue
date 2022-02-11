c6b2816cffb0728b8c4536ee0793ee3eb1ea8a07
Diana Martinez <diananerdoficial@gmail.com>
2022-02-10 18:43:00 -0600

    Add pointer

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <svg
+            @touchstart="tap"
+            @touchmove="tap"
+            @touchend="untap"
             viewBox="0 0 300 200"
         >
             <line
@@ -18,11 +21,12 @@
                 :points="points"
             />
             <line
+                v-show="showPointer"
                 stroke="#04b500"
                 stroke-width="2"
-                x1="200"
+                :x1="pointer"
                 y1="0"
-                x2="200"
+                :x2="pointer"
                 y2="200"
             />
         </svg>
@@ -32,7 +36,7 @@
 </template>
 
 <script setup>
-import { toRefs, defineProps, computed } from 'vue';
+import { ref, toRefs, defineProps, computed } from 'vue';
 
 const props = defineProps({
     amounts: {
@@ -62,10 +66,24 @@ const points = computed(() => {
     return amounts.value.reduce((points, amount, i) => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
-        console.log(y);
         return `${points} ${x},${y}`;
     }, "0, 100");
 });
+
+const showPointer = ref(false);
+const pointer = ref(0);
+
+const tap = ({ target, touches }) => {
+    showPointer.value = true;
+    const elementWidth = target.getBoundingClientRect().width;
+    const elementX = target.getBoundingClientRect().x;
+    const touchX = touches[0].clientX;
+    pointer.value = ((touchX - elementX) * 300) / elementWidth;
+}
+
+const untap = () => {
+    showPointer.value = false;
+}
 </script>
 
 <style scoped>
