65579c05660133826d6ddd129f50bb19afa3f345
Diana Martinez <diananerdoficial@gmail.com>
2022-02-11 19:49:29 -0600

    Bugfix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,7 +11,7 @@
         :amount="amount"
       >
         <template #graphic>
-          <Graphic :amounts="amounts" />
+          <Graphic :amounts="amounts" @select="select" />
         </template>
         <template #action>
           <Action @create="create" />
@@ -61,9 +61,9 @@ export default {
           return m.time > oldDate;
         })
         .map(m => m.amount);
-      
+
       return lastDays.map((m, i) => {
-        const lastMovements = lastDays.slice(0, i);
+        const lastMovements = lastDays.slice(0, i + 1);
 
         return lastMovements.reduce((suma, movement) => {
           return suma + movement
@@ -98,6 +98,10 @@ export default {
     },
     save() {
       localStorage.setItem("movements", JSON.stringify(this.movements));
+    },
+    select(el) {
+      console.log(el);
+      this.amount = el;
     }
   }
 };
--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -35,7 +35,7 @@
 </template>
 
 <script setup>
-import { ref, toRefs, defineProps, computed } from 'vue';
+import { ref, toRefs, defineProps, defineEmits, computed, watch } from 'vue';
 
 const props = defineProps({
     amounts: {
@@ -66,11 +66,19 @@ const points = computed(() => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
         return `${points} ${x},${y}`;
-    }, "0, 100");
+    }, `0, ${amountToPixels(amounts.value.length ? amounts.value[0] : 0)}`);
 });
 
 const showPointer = ref(false);
 const pointer = ref(0);
+
+const emit = defineEmits(["select"]);
+
+watch(pointer, (value) => {
+    const index = Math.ceil((value / (300 / amounts.value.length)));
+    if (index < 0 || index > amounts.value.length) return;
+    emit("select", amounts.value[index - 1]);
+});
 
 const tap = ({ target, touches }) => {
     showPointer.value = true;
